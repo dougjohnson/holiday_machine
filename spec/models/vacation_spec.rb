0c813322802cccfc8f3a13e61d7cb98dff6ba10a
Eamon Skelly <eamon_skelly@hotmail.com>
2011-05-11 21:17:41 +0100

    Tidy up

--- spec/models/vacation_spec.rb
+++ spec/models/vacation_spec.rb
@@ -2,4 +2,17 @@ require 'spec_helper'
 
 describe Vacation do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @vacation = Vacation.new
+  end
+
+  it "should raise error if holiday has passed unless pending"
+
+  it "should ensure date from must be before date to" do
+    pending
+    @vacation.date_from = "2011-02-01"
+    @vacation.date_to = "2011-02-01"
+  end
+
 end
